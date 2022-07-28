1f66f4e7a931c5ae3b7bcd7d0b87e785356ec3fb
DmitriyGovosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-07-28 14:08:45 +0000

    Deploying to gh-pages from @ DmitriyGovosyants/goit-react-hw-06-phonebook@37b2b9c9e96fc3478b51cc0a80641aa077ef50b8 🚀

--- src/components/Container/Container.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  max-width: 400px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: ${p => p.theme.spacing(5)};
-  padding-right: ${p => p.theme.spacing(5)};
-`;
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export * from './App/App';
-export * from './Container/Container.styled';
-export * from './ContactForm/ContactForm';
-export * from './Filter/Filter';
-export * from './ContactList/ContactList';
-export * from './ContactItem/ContactItem';
